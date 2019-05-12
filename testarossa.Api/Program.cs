a05fbfd87ee36b7e2a6ae052626fe77149d5828b
p89 <pawel.prochal@gmail.com>
2019-05-12 20:11:40 +0200

    test

--- testarossa.Api/Program.cs
+++ testarossa.Api/Program.cs
@@ -19,6 +19,6 @@ namespace testarossa.Api
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+                .UseStartup<Startup>();  
     }
 }
