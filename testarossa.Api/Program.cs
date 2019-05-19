08d535798923623ab31e4e336d77761b7e33c528
p89 <pawel.prochal@gmail.com>
2019-05-19 09:01:13 +0200

    ignore

--- testarossa.Api/Program.cs
+++ testarossa.Api/Program.cs
@@ -19,6 +19,9 @@ namespace testarossa.Api
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .UseKestrel()
+                .UseIISIntegration()
                 .UseStartup<Startup>();  
+                
     }
 }
